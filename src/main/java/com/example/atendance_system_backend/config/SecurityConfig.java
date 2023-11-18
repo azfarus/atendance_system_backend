16eddc2e621caf33621c44aee2292aecabb8b17b
Samnun Azfar <samazfar52@gmail.com>
2023-11-18 16:07:37 +0600

    origin added

--- src/main/java/com/example/atendance_system_backend/config/SecurityConfig.java
+++ src/main/java/com/example/atendance_system_backend/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://127.0.0.1:5501"); // Allow requests from any origin (not recommended for production)
 //        configuration.addAllowedOrigin("http://192.168.1.72:5501"); // Allow requests from any origin (not recommended for production)
-        configuration.addAllowedOrigin("*");
+       // configuration.addAllowedOrigin("*");
         configuration.addAllowedMethod("*"); // Allow all HTTP methods
         configuration.addAllowedHeader("*"); // Allow all headers
         configuration.setAllowCredentials(false); // Allow credentials (e.g., cookies)
